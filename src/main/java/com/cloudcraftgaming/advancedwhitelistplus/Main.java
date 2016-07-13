e4cfd62da19973ff225097dc00844b5a8a08b171
NovaFox161 <s.phoenix161@gmail.com>
2016-07-12 19:22:28 -0500

    Initial commit

---
+++ src/main/java/com/cloudcraftgaming/advancedwhitelistplus/Main.java
@@ -0,0 +1,22 @@
+package com.cloudcraftgaming.advancedwhitelistplus;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+/**
+ * Created by Nova Fox on 7/12/2016.
+ * Website: www.cloudcraftgaming.com
+ * For Project: AdvancedWhiteList
+ */
+public class Main extends JavaPlugin {
+    static Main plugin;
+
+    @Override
+    public void onDisable() {}
+
+    @Override
+    public void onEnable() {
+        plugin = this;
+
+
+    }
+}
